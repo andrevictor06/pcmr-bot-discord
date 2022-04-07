ed575d542f7286a3c5e9af6f994cfd0e11cc277c
André <andre.victorueg@gmail.com>
2022-04-06 22:10:31 -0300

    Atualização: Adicionado a funcionalidade de Iniciar uma sala no Watch2Gether

--- function/upload_googledrive.js
+++ function/upload_googledrive.js
@@ -1,19 +1,50 @@
 const request = require('request').defaults({ encoding: null });
 
 function run( bot, msg ){
+    const listFiles = [];
+    //console.log("msg.content 2 ", msg);
     const args = msg.content.split(" ");
-    const url = process.env.URL_APPLETS + "subreddit/" + args[1];
-    console.log(url);
-    request.post(url, function( error, response, body){
-        if (!error && response.statusCode == 200) {
-            const data = Buffer.from(body).toString('utf8');
-            msg.channel.send( data );
-        }
-    });
+    if( msg.attachments){
+        msg.attachments.forEach(attachment => {            
+            console.log("attachment", attachment);
+            listFiles.push({
+                "url" :     attachment.url,
+                "title":    attachment.name
+            })
+        });  
+    }
+
+    if( msg.embeds){
+        msg.embeds.forEach(embed => {
+           // console.log("embed", embed);
+            listFiles.push({
+                "url" :  embed.url,
+                "title": embed.title
+            });
+        });
+    }
+
+    const req_body = {
+        "files" : listFiles
+    }
+    const str_req_body = JSON.stringify(req_body);
+    console.log(str_req_body, " body");
+    if( args[1]){
+        const url = process.env.URL_APPLETS + "upload";
+        request.post({
+            url: url,
+            body: str_req_body
+        }, function( error, response, body){
+            if (!error && response.statusCode == 200) {
+                const data = Buffer.from(body).toString('utf8');
+                msg.channel.send( data );
+            }
+        });
+    }
 }
 
 function canHandle( bot, msg ){
-    return (msg.content.startsWith("/subreddit "));
+    return (msg.content.startsWith("/subreddit"));
 }
 
 module.exports =  {
--- function/video_play.js
+++ function/video_play.js
@@ -1,96 +1,46 @@
 const ytdl = require("ytdl-core");
-const queue = new Map();
+const request = require('request').defaults({ encoding: null });
 
-async function execute(message, serverQueue) {
+async function execute(message) {
     const args = message.content.split(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send("You need to be in a voice channel to play music!");
-
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send("I need the permissions to join and speak in your voice channel!");
-    }
-
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
-    };
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            connection.voice.setSelfDeaf(true);
-            queueContruct.connection = connection;
-            play(message.guild, queueContruct.songs[0]);
-        } catch (err) {+
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
-        }
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(`${song.title} has been added to the queue!`);
+    if( args [1] ){
+        request.post({
+            url:"https://w2g.tv/rooms/create.json",
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "w2g_api_key": process.env.W2G_API_KEY,
+                "share": args [1],
+                "bg_color": "#000000",
+                "bg_opacity": "100"
+            })
+        }, ( error, response, body)=>{
+            if (!error && response.statusCode == 200) {
+                dowloaded = JSON.parse(Buffer.from(body).toString('utf8'));
+                message.channel.send( "Sala no Watch2Gether criada com sucesso!! Para participar acesse: https://w2g.tv/rooms/" + dowloaded.streamkey);
+                //console.log("W2G: Here is your room! \n https://w2g.tv/rooms/" + dowloaded.streamkey);
+            }
+           /* .then(response => response.json())
+            .then(function (data) {
+                console.log("W2G: Here is your room! \n https://w2g.tv/rooms/" + data.streamkey);
+            });*/
+        });
+        
     }
 }
 
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        stop(guild, serverQueue);
-        return;
-    }
-
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
-
-function stop(guild, serverQueue) {
-    if (serverQueue && serverQueue.voiceChannel) {
-        serverQueue.voiceChannel.leave();
-    }
-    if (guild) {
-        queue.delete(guild.id);
-    }
-
-}
 
 function run(bot, msg) {
-    const serverQueue = queue.get(msg.guild.id);
-
     if (msg.content.startsWith("/video ") || msg.content.startsWith("*video ")) {
-        execute(msg, serverQueue)
-    }
-
-    if (msg.content.startsWith("/videostop")) {
-        stop(msg.guild, serverQueue);
+        execute(msg)
     }
 }
 
 function canHandle(bot, msg) {
-    return (msg.content.startsWith("/video ") || msg.content.startsWith("*video ") || msg.content.startsWith("/videostop"));
+    return (msg.content.startsWith("/video ") || msg.content.startsWith("*video "));
 }
 
 module.exports = {
