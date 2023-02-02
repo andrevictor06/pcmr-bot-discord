a81092b21d0cdaa37f38718fd4b0d855cf0efc10
André Victor <andre.victorueg@gmail.com>
2023-02-02 18:40:48 -0300

    Atualizacao do comando de logs do sistema

--- functions/adm_log.js
+++ functions/adm_log.js
@@ -18,31 +18,33 @@ function run(bot, msg){
     const logPath = path.resolve(process.env.PATH_LOG)
     const message = {};
     const logs = []
-    fs.readdirSync(logPath).forEach(file => {
-        logs.push({
-            log: file,
-            type: 1,
-            components: [
-                {
-                    type: 2,
-                    label: file,
-                    style: 1,
-                    custom_id: process.env.ENVIRONMENT + "adm_log" + file,
-                }
-            ]
-        })
-    })
-    if( ! logs){
+    const path_logs = fs.readdirSync(logPath)
+    
+    if( ! path_logs){
         msg.reply({
             content: "Não tem log, corno"
         })
     }else{
-        if( logs.length == 1){
+        if( path_logs.length == 1){
             msg.reply({
                 content: "Ta na mão, corno",
-                files:[path.resolve(logPath, logs[0].log)]
+                files:[path.resolve(logPath, path_logs[0])]
             })    
         }else{
+            path_logs.forEach(file => {
+                logs.push({
+                    log: file,
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: file,
+                            style: 1,
+                            custom_id: process.env.ENVIRONMENT + "adm_log" + file,
+                        }
+                    ]
+                })
+            })
             msg.reply({
                 content: "Tem esses logs, corno",
                 components:logs
