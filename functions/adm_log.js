2883a0e96c9444c51bc5df2262f8c5f44a690ec0
André Victor <andre.victorueg@gmail.com>
2023-02-02 09:49:00 -0300

    Adicionado um comando para baixar o log de erros do servidor

---
+++ functions/adm_log.js
@@ -0,0 +1,28 @@
+const Utils = require("../utils/Utils")
+const fs = require('fs')
+const path = require("path")
+
+function canHandle(bot, msg) {
+    return msg.channel.id == process.env.ID_CHANNEL_LOG_BOT && msg.content.startsWith(Utils.command("adm_log"))
+}
+
+function helpComand(bot, msg) {
+    return {
+        name: Utils.command("adm_log"),
+        value: "Retorna o log de erro do sistema",
+        inline: false
+    }
+}
+
+function run(bot, msg){
+    const audioPath = path.resolve(process.env.PATH_LOG)
+    console.log(audioPath);
+    msg.reply({
+        content: "Ta na mão, corno",
+        files:[audioPath]
+    })
+    console.log("bot, msg");
+}
+module.exports = {
+    run, canHandle, helpComand
+}
