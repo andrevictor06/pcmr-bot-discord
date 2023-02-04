384b932a24c8685c706e43806a3325a88e7d8bd1
Jonas Souza <jonas.souza@grupocompeti.com.br>
2023-02-04 14:40:44 -0300

    Criando funções para os mocks para reduzir tamanho do código e repetições

--- tests/functions/music_play.test.js
+++ tests/functions/music_play.test.js
@@ -1,8 +1,8 @@
 const { expect, test } = require("@jest/globals")
 const { run } = require("../../functions/music_play")
-const Utils = require("../../utils/Utils")
 const { setSharedVariable, AUDIO_QUEUE_NAME, sharedVariableExists, MUSIC_QUEUE_NAME, clearSharedVariables, getSharedVariable } = require("../../utils/shared_variables")
 const { joinVoiceChannel, createAudioPlayer, AudioPlayerStatus } = require("@discordjs/voice")
+const Utils = require("../../utils/Utils")
 const playdl = require('play-dl')
 
 jest.mock('@discordjs/voice')
@@ -11,290 +11,164 @@ jest.useFakeTimers()
 
 beforeEach(() => {
     clearSharedVariables()
+    jest.clearAllMocks()
 })
 
+function mockMessage(command, ...params) {
+    const has = jest.fn(() => true)
+    return {
+        content: Utils.command(command) + (params && params.length > 0 ? " " + params.join(" ") : ""),
+        client: {
+            user: {}
+        },
+        channel: {
+            send: jest.fn()
+        },
+        member: {
+            voice: {
+                channel: {
+                    permissionsFor: jest.fn(() => {
+                        return { has }
+                    }),
+                    guild: {
+                        id: "id",
+                        voiceAdapterCreator: {}
+                    },
+                    members: [{}]
+                }
+            }
+        }
+    }
+}
+
+function mockVoiceConnection() {
+    const connection = { subscribe: jest.fn() }
+    joinVoiceChannel.mockImplementation(() => connection)
+    return connection
+}
+
+function mockAudioPlayer(state = AudioPlayerStatus.Idle) {
+    const player = {
+        on: jest.fn(),
+        state,
+        play: jest.fn()
+    }
+    createAudioPlayer.mockImplementation(() => player)
+    return player
+}
+
+function mockBasicInfo(url, title) {
+    const basicInfo = {
+        video_details: { url, title }
+    }
+    playdl.video_basic_info.mockImplementation(async () => basicInfo)
+    return basicInfo
+}
+
+function mockPlaylistInfo(videos) {
+    const playlistInfo = {
+        all_videos: async () => {
+            return videos
+        }
+    }
+    playdl.playlist_info.mockImplementation(async () => playlistInfo)
+    return playlistInfo
+}
+
 describe("play", () => {
-    test("não deveria iniciar a música quando o usuário não estiver em um canal de voz", () => {
-        const send = jest.fn()
-        const message = {
-            content: Utils.command("play"),
-            channel: { send },
-            member: {
-                voice: {}
-            }
-        }
+    test("não deveria iniciar a música quando o usuário não estiver em um canal de voz", async () => {
+        const message = mockMessage("play")
+        delete message.member.voice.channel
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Cadê o canal de voz?")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Cadê o canal de voz?")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 
-    test("não deveria iniciar a música quando bot não tiver as permissões", () => {
-        const send = jest.fn()
-        const has = jest.fn(str => false)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const message = {
-            content: Utils.command("play"),
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: { permissionsFor }
-                }
-            }
-        }
+    test("não deveria iniciar a música quando bot não tiver as permissões", async () => {
+        const message = mockMessage("play")
+        message.member.voice.channel.permissionsFor().has.mockImplementation(() => false)
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Tô sem permissão, fala com o corno do adm!")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Tô sem permissão, fala com o corno do adm!")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 
-    test("não deveria iniciar a música quando o usuário não informar a música", () => {
-        const send = jest.fn()
-        const has = jest.fn(str => true)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const message = {
-            content: Utils.command("play"),
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: { permissionsFor }
-                }
-            }
-        }
+    test("não deveria iniciar a música quando o usuário não informar a música", async () => {
+        const message = mockMessage("play")
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Cadê a música man?")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Cadê a música man?")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 
     test("não deveria tocar uma música quando tiver um áudio tocando", () => {
-        const send = jest.fn()
-        const has = jest.fn(str => true)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const message = {
-            content: Utils.command("play") + " musica",
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: { permissionsFor }
-                }
-            }
-        }
+        const message = mockMessage("play", "musica")
         setSharedVariable(AUDIO_QUEUE_NAME, {})
 
         run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Tem um áudio tocando man, calma ae")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Tem um áudio tocando man, calma ae")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 
     test("deveria tocar uma música com link de parâmetro", async () => {
         const url = "https://www.youtube.com/watch?v=kijpcUv-b8M"
-        const send = jest.fn()
-        const has = jest.fn(str => true)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const subscribe = jest.fn()
-        const on = jest.fn(() => createAudioPlayer)
-        const play = jest.fn()
-        const message = {
-            content: Utils.command("play") + " " + url,
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: {
-                        permissionsFor,
-                        guild: {
-                            id: "id",
-                            voiceAdapterCreator: {}
-                        },
-                        members: [1]
-                    }
-                }
-            }
-        }
-        joinVoiceChannel.mockImplementation(() => {
-            return { subscribe }
-        })
-        createAudioPlayer.mockImplementation(() => {
-            return {
-                on,
-                state: AudioPlayerStatus.Idle,
-                play
-            }
-        })
-        playdl.stream.mockImplementation(async () => {
-            return { stream: {} }
-        })
-        playdl.video_basic_info.mockImplementation(async () => {
-            return {
-                video_details: {
-                    url,
-                    title: 'titulo'
-                }
-            }
-        })
+        const player = mockAudioPlayer()
+        const message = mockMessage("play", url)
+        mockVoiceConnection()
+        playdl.stream.mockImplementation(async () => ({ stream: {} }))
+        mockBasicInfo(url, "titulo")
 
         await run(null, message)
 
-        expect(play).toBeCalledTimes(1)
-        expect(send).toBeCalledTimes(1)
-        expect(on).toBeCalledTimes(2)
+        expect(player.play).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(player.on).toBeCalledTimes(2)
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeTruthy()
     })
 
     test("deveria tocar uma música com texto", async () => {
         const url = "https://www.youtube.com/watch?v=kijpcUv-b8M"
-        const send = jest.fn()
-        const has = jest.fn(str => true)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const subscribe = jest.fn()
-        const on = jest.fn(() => createAudioPlayer)
-        const play = jest.fn()
-        const message = {
-            content: Utils.command("play") + " queen",
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: {
-                        permissionsFor,
-                        guild: {
-                            id: "id",
-                            voiceAdapterCreator: {}
-                        },
-                        members: [1]
-                    }
-                }
-            }
-        }
-        joinVoiceChannel.mockImplementation(() => {
-            return { subscribe }
-        })
-        createAudioPlayer.mockImplementation(() => {
-            return {
-                on,
-                state: AudioPlayerStatus.Idle,
-                play
-            }
-        })
-        playdl.stream.mockImplementation(async () => {
-            return { stream: {} }
-        })
-        playdl.video_basic_info.mockImplementation(async () => {
-            return {
-                video_details: {
-                    url,
-                    title: 'titulo'
-                }
-            }
-        })
+        const player = mockAudioPlayer()
+        const message = mockMessage("play", "queen")
+        mockVoiceConnection()
+        mockBasicInfo(url, "titulo")
+        playdl.stream.mockImplementation(async () => ({ stream: {} }))
         playdl.search.mockImplementation(async () => {
             return [{ url }]
         })
 
         await run(null, message)
 
-        expect(play).toBeCalledTimes(1)
-        expect(send).toBeCalledTimes(1)
-        expect(on).toBeCalledTimes(2)
+        expect(player.play).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(player.on).toBeCalledTimes(2)
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeTruthy()
     })
 
     test("deveria tocar uma música com link de playlist", async () => {
         const url = "https://www.youtube.com/watch?v=u9Dg-g7t2l4&list=PLX8S4ptxX3CHezw1JDnwAH7CZLFGpj0z-"
         const videos = [{ url }, { url }, { url }]
-        const send = jest.fn()
-        const has = jest.fn(str => true)
-        const permissionsFor = jest.fn(() => {
-            return { has }
-        })
-        const subscribe = jest.fn()
-        const on = jest.fn(() => createAudioPlayer)
-        const play = jest.fn()
-        const message = {
-            content: Utils.command("play") + " " + url,
-            client: {
-                user: {}
-            },
-            channel: { send },
-            member: {
-                voice: {
-                    channel: {
-                        permissionsFor,
-                        guild: {
-                            id: "id",
-                            voiceAdapterCreator: {}
-                        },
-                        members: [1]
-                    }
-                }
-            }
-        }
-        joinVoiceChannel.mockImplementation(() => {
-            return { subscribe }
-        })
-        createAudioPlayer.mockImplementation(() => {
-            return {
-                on,
-                state: AudioPlayerStatus.Idle,
-                play
-            }
-        })
-        playdl.stream.mockImplementation(async () => {
-            return { stream: {} }
-        })
-        playdl.video_basic_info.mockImplementation(async () => {
-            return {
-                video_details: {
-                    url,
-                    title: 'titulo'
-                }
-            }
-        })
-        playdl.playlist_info.mockImplementation(() => {
-            return {
-                all_videos: async () => {
-                    return videos
-                }
-            }
-        })
+        const player = mockAudioPlayer()
+        const message = mockMessage("play", url)
+        mockVoiceConnection()
+        mockBasicInfo(url, "titulo")
+        mockPlaylistInfo(videos)
+        playdl.stream.mockImplementation(async () => ({ stream: {} }))
 
         await run(null, message)
 
-        expect(play).toBeCalledTimes(1)
-        expect(send).toBeCalledTimes(2)
-        expect(on).toBeCalledTimes(2)
+        expect(player.play).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledTimes(2)
+        expect(player.on).toBeCalledTimes(2)
         expect(getSharedVariable(MUSIC_QUEUE_NAME).songs.length).toEqual(videos.length - 1)
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeTruthy()
     })
@@ -316,14 +190,10 @@ describe("stop", () => {
     })
 
     test("deveria parar com sucesso", async () => {
-        const send = jest.fn()
         const removeAllListeners = jest.fn()
         const destroy = jest.fn()
         const stop = jest.fn()
-        const message = {
-            content: Utils.command("stop"),
-            channel: { send }
-        }
+        const message = mockMessage("stop")
         const serverQueue = {
             player: { removeAllListeners, stop },
             connection: { destroy }
@@ -332,7 +202,7 @@ describe("stop", () => {
 
         await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledTimes(1)
         expect(removeAllListeners).toBeCalledTimes(1)
         expect(destroy).toBeCalledTimes(1)
         expect(stop).toBeCalledTimes(1)
@@ -340,14 +210,10 @@ describe("stop", () => {
     })
 
     test("não deveria desconectar quando tiver um áudio rodando", async () => {
-        const send = jest.fn()
         const removeAllListeners = jest.fn()
         const destroy = jest.fn()
         const stop = jest.fn()
-        const message = {
-            content: Utils.command("stop"),
-            channel: { send }
-        }
+        const message = mockMessage("stop")
         const serverQueue = {
             player: { removeAllListeners, stop },
             connection: { destroy }
@@ -357,7 +223,7 @@ describe("stop", () => {
 
         await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
+        expect(message.channel.send).toBeCalledTimes(1)
         expect(removeAllListeners).toBeCalledTimes(1)
         expect(destroy).toBeCalledTimes(0)
         expect(stop).toBeCalledTimes(1)
@@ -366,49 +232,37 @@ describe("stop", () => {
 })
 
 describe("skip", () => {
-    test("não deveria executar se o comando play não estiver rodando", () => {
-        const send = jest.fn()
-        const message = {
-            content: Utils.command("skip"),
-            channel: { send }
-        }
+    test("não deveria executar se o comando play não estiver rodando", async () => {
+        const message = mockMessage("skip")
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Nem tô na sala man")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Nem tô na sala man")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 })
 
 describe("next", () => {
-    test("não deveria executar se o comando play não estiver rodando", () => {
-        const send = jest.fn()
-        const message = {
-            content: Utils.command("next"),
-            channel: { send }
-        }
+    test("não deveria executar se o comando play não estiver rodando", async () => {
+        const message = mockMessage("next")
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Nem tô na sala man")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Nem tô na sala man")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 })
 
 describe("current", () => {
-    test("não deveria executar se o comando play não estiver rodando", () => {
-        const send = jest.fn()
-        const message = {
-            content: Utils.command("current"),
-            channel: { send }
-        }
+    test("não deveria executar se o comando play não estiver rodando", async () => {
+        const message = mockMessage("current")
 
-        run(null, message)
+        await run(null, message)
 
-        expect(send).toBeCalledTimes(1)
-        expect(send).toHaveBeenCalledWith("Nem tô na sala man")
+        expect(message.channel.send).toBeCalledTimes(1)
+        expect(message.channel.send).toHaveBeenCalledWith("Nem tô na sala man")
         expect(sharedVariableExists(MUSIC_QUEUE_NAME)).toBeFalsy()
     })
 })
