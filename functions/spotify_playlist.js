298a809cbf6cbbb5dc32a202f92830eeb65d9940
jonasgsouza <jonas.gdsouza@gmail.com>
2023-04-02 19:13:28 +0000

    Integração com o spotify

--- functions/spotify_playlist.js
+++ functions/spotify_playlist.js
@@ -1,23 +1,32 @@
-const { MUSIC_PLAY_SONG_EVENT, SPOTIFY_LOGIN_STATE, SPOTIFY_LOGIN_CODE, SPOTIFY_TOKEN, SPOTIFY_TOKEN_EXPIRATION, SPOTIFY_REFRESH_TOKEN } = require('../utils/constants')
+const { MUSIC_PLAY_SONG_EVENT, SPOTIFY_LOGIN_STATE, SPOTIFY_LOGIN_CODE, SPOTIFY_TOKEN, SPOTIFY_TOKEN_EXPIRATION, SPOTIFY_REFRESH_TOKEN, SPOTIFY_AUTH_URL, SPOTIFY_BASE_URL, SPOTIFY_PLAYLIST_TRACKS } = require('../utils/constants')
 const events = require('../utils/events');
-const { setSharedVariable } = require('../utils/shared_variables');
+const { setSharedVariable, getSharedVariable } = require('../utils/shared_variables');
 const utils = require('../utils/Utils')
 const { default: axios } = require('axios')
 const { randomUUID } = require('crypto');
 const querystring = require('querystring')
-const localStorage = require('../utils/localstorage')
+const localStorage = require('../utils/localstorage');
 
-const spotifyEndpoint = 'https://api.spotify.com/v1'
+const commands = {
+    spotify_login: {
+        fn: spotifyLogin
+    },
+    spotify_cache: {
+        fn: spotifyCache
+    }
+}
 
 async function getAuthToken() {
     const refreshToken = localStorage.getItem(SPOTIFY_REFRESH_TOKEN)
     if (refreshToken) {
         const tokenExpirationEpoch = localStorage.getItem(SPOTIFY_TOKEN_EXPIRATION)
         if (epochTimeInSecond() >= Number.parseInt(tokenExpirationEpoch ? tokenExpirationEpoch : 0)) {
+            console.log('Atualizando token...')
             const response = await refreshAccessToken(refreshToken)
             saveNewCredentials(response)
             return response.data.access_token
         }
+        console.log('Retornando token do cache...')
         return localStorage.getItem(SPOTIFY_TOKEN)
     } else {
         console.log('Autenticando...')
@@ -43,7 +52,7 @@ function deleteCredentials() {
 
 function authenticate() {
     return axios.post(
-        'https://accounts.spotify.com/api/token',
+        SPOTIFY_AUTH_URL,
         {
             grant_type: 'authorization_code',
             redirect_uri: `${process.env.SPOTIFY_CALLBACK_URL}/spotify_login`,
@@ -52,7 +61,7 @@ function authenticate() {
         {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: 'Basic ' + Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+                Authorization: 'Basic ' + basicAuth()
             }
         }
     )
@@ -60,7 +69,7 @@ function authenticate() {
 
 function refreshAccessToken(refreshToken) {
     return axios.post(
-        'https://accounts.spotify.com/api/token',
+        SPOTIFY_AUTH_URL,
         {
             grant_type: 'refresh_token',
             refresh_token: refreshToken
@@ -68,7 +77,7 @@ function refreshAccessToken(refreshToken) {
         {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: 'Basic ' + Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+                Authorization: 'Basic ' + basicAuth()
             }
         }
     )
@@ -76,7 +85,7 @@ function refreshAccessToken(refreshToken) {
 
 async function search(q) {
     return axios.get(
-        `${spotifyEndpoint}/search`,
+        `${SPOTIFY_BASE_URL}/search`,
         {
             headers: {
                 Authorization: `Bearer ${await getAuthToken()}`
@@ -90,9 +99,33 @@ async function search(q) {
     )
 }
 
+async function getAllTracksFromPlaylist(limit, offset = 0, tracks = []) {
+    const response = await axios.get(
+        `${SPOTIFY_BASE_URL}/playlists/${process.env.SPOTIFY_URSAL_PLAYLIST_ID}/tracks`,
+        {
+            headers: {
+                Authorization: `Bearer ${await getAuthToken()}`
+            },
+            params: {
+                fields: `items.track.id`,
+                limit,
+                offset
+            }
+        }
+    )
+    if (response.data?.items && response.data.items.length > 0) {
+        return getAllTracksFromPlaylist(
+            limit,
+            limit + offset,
+            tracks.concat(response.data.items.map(i => i.track.id))
+        )
+    }
+    return tracks
+}
+
 async function addToPlaylist(track) {
     return axios.post(
-        `${spotifyEndpoint}/playlists/${process.env.SPOTIFY_URSAL_PLAYLIST_ID}/tracks`,
+        `${SPOTIFY_BASE_URL}/playlists/${process.env.SPOTIFY_URSAL_PLAYLIST_ID}/tracks`,
         {
             uris: [track.uri]
         },
@@ -104,14 +137,6 @@ async function addToPlaylist(track) {
     )
 }
 
-async function init(bot) {
-    events.event(MUSIC_PLAY_SONG_EVENT)
-        .subscribe({
-            next: song => tryAddSongToSpotifyPlaylist(bot, song),
-            error: () => utils.logError(bot, error, __filename)
-        })
-}
-
 async function tryAddSongToSpotifyPlaylist(bot, song) {
     try {
         console.log(song.video_details.music)
@@ -120,14 +145,38 @@ async function tryAddSongToSpotifyPlaylist(bot, song) {
             console.log('Música sem info')
             return
         }
-        const response = await search(info.song)
-        console.log(response.data)
-        const track = response.data?.tracks?.items[0]
-        console.log(track.uri)
-        await addToPlaylist(track)
+        const tracksCache = getSharedVariable(SPOTIFY_PLAYLIST_TRACKS)
+        if (!tracksCache) throw new Error('Não foi realizado o cache das músicas')
+
+        const trackSearchResponse = await search(`track:${info.song} artist:${info.artist}`)
+        console.log(trackSearchResponse.data)
+        if (trackSearchResponse.data?.tracks?.items && trackSearchResponse.data.tracks.items[0]) {
+            const track = trackSearchResponse.data.tracks.items[0];
+            if (tracksCache.includes(track.id)) {
+                console.log('Música já existe na playlist')
+                return
+            }
+            await addToPlaylist(track)
+        }
     } catch (error) {
         error = error && error.response.data ? error.response.data : error
         utils.logError(bot, error, __filename)
+    }
+}
+
+async function init(bot) {
+    events.event(MUSIC_PLAY_SONG_EVENT)
+        .subscribe({
+            next: song => tryAddSongToSpotifyPlaylist(bot, song),
+            error: () => utils.logError(bot, error, __filename)
+        })
+    loadTracksCache()
+}
+
+function loadTracksCache() {
+    const tracksStr = localStorage.getItem(SPOTIFY_PLAYLIST_TRACKS)
+    if (tracksStr) {
+        setSharedVariable(SPOTIFY_PLAYLIST_TRACKS, JSON.parse(tracksStr))
     }
 }
 
@@ -135,7 +184,11 @@ function epochTimeInSecond() {
     return Number.parseInt(new Date().getTime() / 1000)
 }
 
-function run(bot, msg) {
+function basicAuth() {
+    return Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+}
+
+function spotifyLogin(bot, msg) {
     const state = randomUUID()
     const scope = 'playlist-modify-private playlist-modify-public'
     const qs = querystring.stringify({
@@ -150,8 +203,19 @@ function run(bot, msg) {
     bot.users.cache.get(msg.author.id).send(`https://accounts.spotify.com/authorize?${qs}`)
 }
 
+async function spotifyCache(bot, msg) {
+    const tracks = await getAllTracksFromPlaylist(50)
+    localStorage.setItem(SPOTIFY_PLAYLIST_TRACKS, JSON.stringify(tracks))
+    loadTracksCache()
+    msg.reply(`Cache das ${tracks.length} músicas da playlist do spotify realizado!`)
+}
+
+function run(bot, msg) {
+    return utils.executeCommand(bot, msg, commands)
+}
+
 function canHandle(bot, msg) {
-    return msg.content.startsWith(utils.command('spotify_login'))
+    return utils.containsCommand(msg, commands)
 }
 
 function helpComand(bot, msg) {
--- utils/constants.js
+++ utils/constants.js
@@ -4,6 +4,9 @@ const SPOTIFY_LOGIN_CODE = 'spotify:code'
 const SPOTIFY_TOKEN = 'spotify:token'
 const SPOTIFY_TOKEN_EXPIRATION = 'spotify:token_expiration'
 const SPOTIFY_REFRESH_TOKEN = 'spotify:refresh_token'
+const SPOTIFY_BASE_URL = 'https://api.spotify.com/v1'
+const SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/api/token'
+const SPOTIFY_PLAYLIST_TRACKS = 'spotify:playlist_tracks'
 
 module.exports = {
     MUSIC_PLAY_SONG_EVENT,
@@ -11,5 +14,8 @@ module.exports = {
     SPOTIFY_LOGIN_CODE,
     SPOTIFY_TOKEN,
     SPOTIFY_TOKEN_EXPIRATION,
-    SPOTIFY_REFRESH_TOKEN
+    SPOTIFY_REFRESH_TOKEN,
+    SPOTIFY_BASE_URL,
+    SPOTIFY_AUTH_URL,
+    SPOTIFY_PLAYLIST_TRACKS
 }
