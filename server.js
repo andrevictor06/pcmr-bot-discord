34270fda4578c9e26e364346a681f9a21d00c1b7
Lii <eliezer.oliveirasiueg@gmail.com>
2023-04-04 03:04:03 +0000

    ajustando para pegar rotas dinamicamente - removendo ; de código

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const spotifyRoutes = require('./routes/spotify');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
 
 function init(bot) {
-    const app = express();
+    const app = express()
 
-    initRoutes(app, bot);
+    initRoutes(app, bot)
 
     app.listen(process.env.SERVER_PORT, () => {
-        console.log(`Server UP on port ${process.env.SERVER_PORT}`);
-    });
+        console.log(`Server UP on port ${process.env.SERVER_PORT}`)
+    })
 }
 
 function initRoutes(app, bot) {
-    spotifyRoutes.init(bot);
-    app.use('/spotify', spotifyRoutes.router);
+    try {
+        fs.readdirSync("./routes").forEach((file) => {
+            const route = require(path.join(__dirname, 'routes', file))
+    
+            if (typeof route.init === 'function') {
+                route.init(bot)
+            } else {
+                log.warn(`A rota ${file} não possui uma função 'init'.`)
+            }
+            
+            const routePath = '/' + path.basename(file, path.extname(file))
+            app.use(routePath, route.router)
+        })
+    } catch (error) { Utils.logError(bot, error, __filename) }
 }
 
 module.exports = {
