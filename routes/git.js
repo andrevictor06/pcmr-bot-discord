feb8906f0990da9d77022ae51b933822fdf3bd53
André Victor <andre.victorueg@gmail.com>
2023-04-04 11:49:52 -0300

    Adicionado a rota 'git' para escutar a ação de criar um novo Pull Request

--- routes/git.js
+++ routes/git.js
@@ -1,15 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')   
 const router = express.Router()
 const spotify = require('../functions/spotify_playlist')
 const utils = require('../utils/Utils')
 
+router.use( bodyParser.json())
 function init(bot) {
-    router.post('/git', async (req, res) => {
+    router.post('/', async (req, res) => {
         try {
-            console.log(req, " req");
-            const channel = await bot.channels.fetch(process.env.ID_CHANNEL_LOG_BOT)
-            channel.send({ content: ` ${ req.body  } ` })
+            let payload = req.body
 
+
+            if( payload.action && payload.pull_request && payload.action === "opened"){
+                const channel = await bot.channels.fetch(process.env.ID_CHANNEL_DEV_BOT)
+
+                const exampleEmbed = {
+                    color: 0x0099ff,
+                    author: {
+                        name: payload.sender.login,
+                        icon_url: payload.sender.avatar_url,
+                        url: payload.sender.html_url
+                    },
+
+                    title: 'Abrirão um PR pra Atualizar o Bot!!!!',
+                    url: payload.pull_request.html_url,
+                    description: payload.pull_request.title,
+                    timestamp: new Date(payload.pull_request.created_at).toISOString()    
+                }   
+                
+                channel.send({ embeds: [exampleEmbed] }) 
+            }
+
+            
+            //msg.reply({ embeds: [exampleEmbed] })
             res.send('Requisição aceita')
         } catch (error) {
             utils.logError(bot, error, __filename)
